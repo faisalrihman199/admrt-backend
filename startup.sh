37098b179a2cf6cc8cc949895ca6ace064292c07
faisalrihman199 <129194773+faisalrihman199@users.noreply.github.com>
2024-10-13 22:37:16 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 # python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 admrt.wsgi
 
+source venv/bin/activate
 
-
-python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8080 && python3 manage.py runserver 0.0.0.0:8080 collectstatic --noinput && waitress-serve --listen=0.0.0.0:8080 admrt.wsgi:application
+python manage.py migrate && python manage.py runserver 0.0.0.0:8080 && python manage.py runserver 0.0.0.0:8080 collectstatic --noinput && waitress-serve --listen=0.0.0.0:8080 admrt.wsgi:application
